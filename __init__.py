e83d212c9738be2b2a7af1a2775464da9a3379c6
azmiao <2362020227@qq.com>
2023-10-14 13:59:39 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
-import os
 import json
-import requests
+import os
 
+import requests
 from hoshino import Service
+
 from .binds import get_displayName, bind_displayName, unbind_displayName
-from .kon_search import get_pinfo, get_playing, get_history, get_serverid, get_platoon, get_status, check_easb, check_ban
-from .gamtools_search import get_pinfo_gt, get_status_gt
+from .gamtools_search import get_info_gt, get_status_gt
+from .kon_search import get_exchanges
 
 sv_help = '''
-=====简易战地助手=====
+==== 简易战地助手V2 ====
 
 (※注：绑定昵称后下列查询均可不带昵称)
 
@@ -18,39 +19,15 @@ sv_help = '''
 
 [橘子ID] 查询当前绑定的战地昵称
 
-=====K-ON数据=====
-
-[bf1查询 角色昵称] 查询战地1战绩
-
-[bfv查询 角色昵称] 查询战地5战绩
-
-[bf正在游玩 角色昵称] 查询战地系列的正在游玩的服务器
-
-[bf1最近游玩 角色昵称] 查询战地1的最近游玩过的服务器
-
-[bfv最近游玩 角色昵称] 查询战地5的最近游玩过的服务器
-
-[bf1查服务器 服务器名] 根据服务器名查询该战地1服务器信息
-
-[bfv查服务器 服务器名] 根据服务器名查询该战地5服务器信息
-
-[bf查战队 角色昵称] 查询该玩家的战队信息
-
-[bf查easb 角色昵称] 查询该玩家的easb信息
-
-[bf查联ban 角色昵称] 查询该玩家的联ban信息
-
-[bf数据统计] 仅可查询PC端战地1和战地5的游戏数据统计
-
-=====Gametool数据=====
-
-[战地3查询 角色昵称] 查询战地3战绩
-
-[战地4查询 角色昵称] 查询战地4信息
+[战地1交换] 查询战地1本周交换
 
 [战地1查询 角色昵称] 查询战地1战绩
 
 [战地5查询 角色昵称] 查询战地5信息
+
+[战地3查询 角色昵称] 查询战地3战绩
+
+[战地4查询 角色昵称] 查询战地4信息
 
 [战地数据统计x] 查询PC端战地x的游戏数据统计，x可选3/4/1/5/2042
 
@@ -63,13 +40,15 @@ if not os.path.exists(_current_dir):
     with open(_current_dir, 'w', encoding='UTF-8') as f:
         json.dump({}, f, indent=4, ensure_ascii=False)
 
-sv = Service('simple_bf_search', help_=sv_help, enable_on_default=True, bundle='简易战地查询')
+sv = Service('bf_search_v2', help_=sv_help, enable_on_default=True, bundle='战地查询V2')
+
 
 @sv.on_fullmatch('战地帮助')
 async def get_help(bot, ev):
     await bot.send(ev, sv_help)
 
-# 绑定解绑
+
+# 绑定解绑和查询IDD
 @sv.on_rex(r'^橘子(\S{2}) ?(\S+)?$')
 async def bind_id(bot, ev):
     user_id = str(ev.user_id)
@@ -78,78 +57,49 @@ async def bind_id(bot, ev):
         return
     if operation == '绑定':
         try:
-            displayName = ev['match'].group(2)
+            display_name = ev['match'].group(2)
         except:
-            await bot.finish(ev, '指令格式错误，绑定时请带上昵称')
-        msg = await bind_displayName(user_id, displayName)
+            await bot.send(ev, '指令格式错误，绑定时请带上昵称')
+            return
+        msg = await bind_displayName(user_id, display_name)
     elif operation == '解绑':
         msg = await unbind_displayName(user_id)
     elif operation == 'ID' or operation == 'id':
         msg = await get_displayName(user_id)
         msg = msg if msg else '你还暂未绑定ID'
+    else:
+        return
     await bot.send(ev, msg)
 
-# K-0n查询接口
-@sv.on_rex(r'^bf(\S)(\S{2,4}) ?(\S+)?$')
-async def search_kon(bot, ev):
-    user_id = str(ev.user_id)
-    game = ev['match'].group(1)
-    if game not in ['1', 'v', '5', '正', '查', '数']:
-        return
-    game = 'v' if game == '5' else game
-    search_type = ev['match'].group(2)
-    if search_type not in ['查服务器', '查询', '在游玩', '最近游玩', '查服务器', '战队', 'easb', '联ban', '据统计']:
-        return
-    displayName = ev['match'].group(3)
-    try:
-        if search_type == '据统计':
-            msg = await get_status()
-            await bot.finish(ev, msg)
-        if not displayName:
-            if search_type == '查服务器':
-                await bot.finish(ev, '指令格式错误，查服务器请带上服务器名')
-            else:
-                displayName = await get_displayName(user_id)
-                if not displayName:
-                    await bot.finish(ev, '你当前QQ暂未绑定战地ID')
-        if search_type == '查询':
-            msg = await get_pinfo(game, displayName)
-        elif search_type == '在游玩':
-            msg = await get_playing(displayName)
-        elif search_type == '最近游玩':
-            msg = await get_history(game, displayName)
-        elif search_type == '查服务器':
-            msg = await get_serverid(game, displayName)
-        elif search_type == '战队':
-            msg = await get_platoon(displayName)
-        elif search_type == 'easb':
-            msg = await check_easb(displayName)
-        elif search_type == '联ban':
-            msg = await check_ban(displayName)
-    except requests.exceptions.ReadTimeout as e:
-        msg = f'连接K-ON数据源超时：{e}，请再次尝试'
+
+# 查询战地1本周交换
+@sv.on_fullmatch('战地1交换')
+async def search_exchange(bot, ev):
+    msg = await get_exchanges()
     await bot.send(ev, msg)
 
-# Gametool查询接口
+
+# GameTool查询战绩接口
 @sv.on_rex(r'^战地([0-9])查询 ?(\S+)?$')
 async def search_gametools(bot, ev):
     user_id = str(ev.user_id)
     game = ev['match'].group(1)
     if game not in ['3', '4', '1', '5']:
         return
-    displayName = ev['match'].group(2)
-    if not displayName:
-        displayName = await get_displayName(user_id)
-        if not displayName:
+    display_name = ev['match'].group(2)
+    if not display_name:
+        display_name = await get_displayName(user_id)
+        if not display_name:
             await bot.finish(ev, '你当前QQ暂未绑定战地ID')
     await bot.send(ev, '正在从Gametools查询，请耐心等待')
     try:
-        msg = await get_pinfo_gt(str(game), displayName)
+        msg = await get_info_gt(str(game), display_name)
     except requests.exceptions.ReadTimeout as e:
         msg = f'查询超时：{e}'
     await bot.send(ev, msg)
 
-# Gametool查询接口单独的数据统计
+
+# GameTool查询接口单独的数据统计
 @sv.on_prefix('战地数据统计')
 async def status_gametools(bot, ev):
     game = str(ev.message)
--- gamtools_search.py
+++ gamtools_search.py
@@ -1,43 +1,45 @@
-import requests
-import os
 import asyncio
 import json
+import os
 
+import requests
 from hoshino import R, logger
 
 # 设置代理
-curpath = os.path.dirname(__file__)
+cur_path = os.path.dirname(__file__)
 with open(os.path.join(os.path.dirname(__file__), 'account.json')) as fp:
-    pinfo = json.load(fp)
-proxy = pinfo['proxy']
+    p_info = json.load(fp)
+proxy = p_info['proxy']
+
 
 # 保存头像图片
-async def save_img(avatar, userName):
-    img_path = os.path.join(R.img('bf_search').path, f'{userName}.png')
+async def save_img(avatar, user_name):
+    img_path = os.path.join(R.img('bf_search').path, f'{user_name}.png')
     if not os.path.exists(img_path):
-        resq = requests.get(url = avatar, timeout=15, proxies = proxy)
+        resp = requests.get(url=avatar, timeout=15, proxies=proxy)
         with open(img_path, 'wb') as f:
-            f.write(resq.content)
-        logger.info(f'未检测到橘子头像 {userName}.png ，现已下载成功')
+            f.write(resp.content)
+        logger.info(f'未检测到橘子头像 {user_name}.png ，现已下载成功')
     return os.path.abspath(img_path)
 
+
 # 获取玩家查询的综合信息(战地3或4，1和5在其基础上多了步战数据)
-async def get_pinfo_gt(game, displayName):
+async def get_info_gt(game, display_name):
     game = 'v' if game == '5' else game
     url = f'https://api.gametools.network/bf{game}/stats/'
     params = {
-        'name': displayName,
+        'name': display_name,
         'platform': 'pc',
     }
-    per_info = requests.get(url=url, params=params, timeout=20, proxies = proxy).json()
+    per_info = requests.get(url=url, params=params, timeout=20, proxies=proxy).json()
     if per_info.get('errors'):
-        return  '出现错误！API返回信息：\n' + str(per_info.get('errors'))
+        return '出现错误！API返回信息：\n' + str(per_info.get('errors'))
     avatar = per_info['avatar']
-    userName = per_info['userName']
-    avatar_img = await save_img(avatar, userName)
+    user_name = per_info['userName']
+    avatar_img = await save_img(avatar, user_name)
     msg = f'''
 [CQ:image,file=file:///{avatar_img}]
-昵称：{userName}
+昵称：{user_name}
 等级：{int(per_info['rank'])}
 KD：{per_info['killDeath']}
 KPM：{per_info['killsPerMinute']}
@@ -47,12 +49,13 @@ SPM：{per_info['scorePerMinute']}
 爆头率：{per_info['headshots']}
 总击杀：{int(per_info['kills'])}
 游戏局数：{int(per_info['wins']) + int(per_info['loses'])}
-场均击杀：{round(int(per_info['kills'])/(int(per_info['wins']) + int(per_info['loses'])), 2)}
+场均击杀：{round(int(per_info['kills']) / (int(per_info['wins']) + int(per_info['loses'])), 2)}
 游戏时间：{per_info['timePlayed']}
     '''.strip()
     if game == '1' or game == 'v':
         msg += f'\n步战KD：{per_info["infantryKillDeath"]}\n步战KPM：{per_info["infantryKillsPerMinute"]}'
     return msg
+
 
 # 数据统计
 async def get_status_gt(bf_list):
@@ -62,18 +65,18 @@ async def get_status_gt(bf_list):
         logger.info(f'正在查询bf{game}的数据统计')
         try:
             url = f'https://api.gametools.network/bf{game}/status/'
-            stauts_info = requests.get(url=url, timeout=20, proxies = proxy).json()
+            status_info = requests.get(url=url, timeout=20, proxies=proxy).json()
         except requests.exceptions.ReadTimeout:
             logger.info(f'查询bf{game}超时将跳过')
             msg += f'游戏bf{game}查询超时，已跳过，你可以尝试再次单独查询该游戏'
             continue
         msg += f'''▼游戏bf{game}：
-总在线人数：{int(stauts_info["regions"]["ALL"]["amounts"]["soldierAmount"])}
-等待队列：{int(stauts_info["regions"]["ALL"]["amounts"]["queueAmount"])}
-观看：{int(stauts_info["regions"]["ALL"]["amounts"]["spectatorAmount"])}
-总服务器数：{int(stauts_info["regions"]["ALL"]["amounts"]["serverAmount"])}
+总在线人数：{int(status_info["regions"]["ALL"]["amounts"]["soldierAmount"])}
+等待队列：{int(status_info["regions"]["ALL"]["amounts"]["queueAmount"])}
+观看：{int(status_info["regions"]["ALL"]["amounts"]["spectatorAmount"])}
+总服务器数：{int(status_info["regions"]["ALL"]["amounts"]["serverAmount"])}
 '''
         if game == '1' or game == 'v':
-            msg += f'官服：{int(stauts_info["regions"]["ALL"]["amounts"]["diceServerAmount"])}\n'
-            msg += f'私服：{int(stauts_info["regions"]["ALL"]["amounts"]["communitySoldierAmount"])}\n'
+            msg += f'官服：{int(status_info["regions"]["ALL"]["amounts"]["diceServerAmount"])}\n'
+            msg += f'私服：{int(status_info["regions"]["ALL"]["amounts"]["communitySoldierAmount"])}\n'
     return msg
